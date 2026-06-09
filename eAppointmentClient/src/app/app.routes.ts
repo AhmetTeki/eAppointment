01ae3ee58d0226b4700ec1c116a063c3ed9ddfde
Ahmet Tekin <ahmettekin023402@gmail.com>
2026-06-09 14:30:25 +0300

    Add foundational Angular components, routing, and styles integration

--- eAppointmentClient/src/app/app.routes.ts
+++ eAppointmentClient/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Layouts } from './Components/layouts/layouts';
+import { Home } from './Components/home/home';
+import { Login } from './Components/login/login';
+import { NotFound } from './Components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: '',
+    component: Layouts,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
+];
