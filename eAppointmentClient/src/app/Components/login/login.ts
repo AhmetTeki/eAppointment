c01cc6e3eaf8cd87115d539b76b7989097029b33
Ahmet Tekin <ahmettekin023402@gmail.com>
2026-06-09 16:22:11 +0300

    Integrate `form-validate-angular` for login form validation and bind form inputs to `LoginModel`

--- eAppointmentClient/src/app/Components/login/login.ts
+++ eAppointmentClient/src/app/Components/login/login.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { LoginModel } from '../../Models/login.model';
+import { FormValidateDirective } from 'form-validate-angular';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, FormValidateDirective],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  login: LoginModel = new LoginModel();
+}
