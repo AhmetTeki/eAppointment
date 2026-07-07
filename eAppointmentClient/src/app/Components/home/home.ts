a070c82b78c9dd96b96a43898f1b3352ec33021a
Ahmet Tekin <ahmettekin023402@gmail.com>
2026-07-07 16:46:12 +0300

    Appointment page design (add devextrem)

--- eAppointmentClient/src/app/Components/home/home.ts
+++ eAppointmentClient/src/app/Components/home/home.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { departments } from '../../Constans';
+import { DoctorModel } from '../../Models/doctor.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DxSchedulerModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule, CommonModule, DxSchedulerModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  departments = departments;
+  doctors: DoctorModel[] = [];
 
+  selectedDepartmentValue: number = 0;
+  selectedDoctorId: string = '';
 }
--- eAppointmentServer/eAppointmentServer.Application/Mapping/MappingProfile.cs
+++ eAppointmentServer/eAppointmentServer.Application/Mapping/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using eAppointmentServer.Application.Features.Doctors.CreateDoctor;
 using eAppointmentServer.Application.Features.Doctors.UpdateDoctor;
+using eAppointmentServer.Application.Features.Patients.CreatePatient;
+using eAppointmentServer.Application.Features.Patients.UpdatePatient;
 using eAppointmentServer.Domain.Entities;
 using eAppointmentServer.Domain.Enums;
 
@@ -12,5 +14,7 @@ public class MappingProfile: Profile
     {
         CreateMap<CreateDoctorCommand,Doctor>().ForMember(member => member.Department, opt => opt.MapFrom(command => DepartmentEnum.FromValue(command.Department)));
         CreateMap<UpdateDoctorCommand,Doctor>().ForMember(member => member.Department, opt => opt.MapFrom(command => DepartmentEnum.FromValue(command.Department)));
+        CreateMap<CreatePatientCommand, Patient>();
+        CreateMap<UpdatePatientCommand, Patient>();
     }
 }
